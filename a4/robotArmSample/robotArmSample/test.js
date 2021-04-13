073200308b570dc5634509edbe13d997c00e3a66
tgrabham <tgrabham@sfu.ca>
2021-04-12 22:57:33 -0700

    got the sides, just missing the circles on top

--- a4/robotArmSample/robotArmSample/test.js
+++ a4/robotArmSample/robotArmSample/test.js
@@ -2,132 +2,71 @@
 
 var canvas, gl, program;
 
-var NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
-var NumCylSides = 36;
-
-var points = [];
-var cylpoints = [];
-var colors = [];
-
-var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4(  0.5,  0.5,  0.5, 1.0 ),
-    vec4(  0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4(  0.5,  0.5, -0.5, 1.0 ),
-    vec4(  0.5, -0.5, -0.5, 1.0 )
-];
-
-var cylvertices = [];
-var cylcolors = [];
-
-function initcylvertices(){
-  var x, z;
-  var cyltheta = 0;
-  var delta = Math.PI*2/NumCylSides;
-
-  for(var i=0;i<NumCylSides;i++){
-    cylvertices.push(vec4(Math.cos(cyltheta)/2, 0.5, Math.sin(cyltheta)/2, 1));
-  }
-  cyltheta = 0;
-  for(var i=0;i<NumCylSides;i++){
-    cylvertices.push(vec4(Math.cos(cyltheta)/2, -0.5, Math.sin(cyltheta)/2, 1));
-  }
-}
-
-initcylvertices();
-
-// RGBA colors
-var vertexColors = [
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
-    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-];
-
-
-// Parameters controlling the size of the Robot's arm
-
-var BASE_HEIGHT      = 2.0;
-var BASE_WIDTH       = 5.0;
-var LOWER_ARM_HEIGHT = 2.0;
-var LOWER_ARM_WIDTH  = 0.3;
-var UPPER_ARM_HEIGHT = 2.0;
-var UPPER_ARM_WIDTH  = 0.3;
-
 // Shader transformation matrices
 
 var modelViewMatrix, projectionMatrix;
 
-// Array of rotation angles (in degrees) for each rotation axis
+let points = [];
+let vertices = [];
+let colors = [];
 
-var Base = 0;
-var LowerArm = 1;
-var UpperArm = 2;
-
-
-var theta= [ 0, 0, 0];
-
-var angle = 0;
-
-var modelViewMatrixLoc;
-
-var vBuffer, cBuffer;
-
-//----------------------------------------------------------------------------
-
-function quad(  a,  b,  c,  d ) {
-    colors.push(vertexColors[a]);
-    points.push(vertices[a]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[b]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[c]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[a]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[c]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[d]);
-}
-
-function cylquad(  a,  b,  c,  d ) {
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[a]);
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[b]);
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[c]);
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[a]);
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[c]);
-    cylcolors.push(vec4(1.0,0.0,0.0,1.0));
-    cylpoints.push(cylvertices[d]);
-}
-
-
-function colorCube() {
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-}
-
-function colorCyl() {
-  for(var i=0;i<NumCylSides-1;i++){
-    cylquad(i+1,i, NumCylSides+i, NumCylSides+i+1);
+function initCircleVertices (n){
+  let angle = 0;
+  let inc = Math.PI*2/n;
+  for(var i=0;i<n;i++){
+    vertices.push(vec4(Math.cos(angle), Math.sin(angle), 0, 1));
+    angle += inc;
   }
-  cylquad(0,NumCylSides-1, 2*NumCylSides-2, 2*NumCylSides-1);
 }
+
+function initCircle(){
+  for(var i=0;i<vertices.length-1;i++){
+    points.push(vec4(0,0,0,1));
+    points.push(vertices[i]);
+    points.push(vertices[i+1]);
+  }
+  points.push(vec4(0,0,0,1));
+  points.push(vertices[vertices.length-1]);
+  points.push(vertices[0]);
+  for(var i=0;i<points.length;i++){
+    colors.push(vec4(1,0,0,1));
+  }
+}
+
+function quad(a, b, c, d){
+  points.push(vertices[a]);
+  points.push(vertices[b]);
+  points.push(vertices[c]);
+  points.push(vertices[a]);
+  points.push(vertices[c]);
+  points.push(vertices[d]);
+  for(var i=0;i<6;i++){
+    colors.push(vec4(1,0,0,1));
+  }
+}
+
+function initCylinderVertices(n){
+  let angle = 0;
+  let inc = Math.PI*2/n;
+  for(var i=0;i<n;i++){
+    vertices.push(vec4(Math.cos(angle), 1, Math.sin(angle)-0.5, 1));
+    angle += inc;
+  }
+  angle = 0;
+  for(var i=0;i<n;i++){
+    vertices.push(vec4(Math.cos(angle), 0, Math.sin(angle)-0.5, 1));
+    angle += inc;
+  }
+}
+
+function initCylinder() {
+  let n = vertices.length/2;
+  for(var i=0;i<n-1;i++){
+    quad(i,i+1,i+n+1,i+n);
+  }
+  quad(n-1,0,n,2*n-1);
+}
+
 
 //____________________________________________
 
@@ -164,114 +103,53 @@ window.onload = function init() {
 
     gl.useProgram( program );
 
-    // colorCube();
-    colorCyl();
 
     // Load shaders and use the resulting shader program
 
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    // initCircleVertices(36);
+    // initCircle();
+    initCylinderVertices(36);
+    initCylinder();
+
     // Create and initialize  buffer objects
 
-    vBuffer = gl.createBuffer();
+    let vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cylpoints), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    cBuffer = gl.createBuffer();
+    let cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cylcolors), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 
-    document.getElementById("slider1").onchange = function(event) {
-        theta[0] = event.target.value;
-    };
-    document.getElementById("slider2").onchange = function(event) {
-         theta[1] = event.target.value;
-    };
-    document.getElementById("slider3").onchange = function(event) {
-         theta[2] =  event.target.value;
-    };
 
-    // modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-    //
-    // projectionMatrix = ortho(-10, 10, -10, 10, -10, 10);
-    // gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),  false, flatten(projectionMatrix) );
-    //
-    // modelViewMatrix = rotate(theta[Base], 0, 1, 0 );
-    // var s = scale4(BASE_WIDTH, BASE_HEIGHT, BASE_WIDTH);
-    // var instanceMatrix = mult( translate( 0.0, 0.5 * BASE_HEIGHT, 0.0 ), s);
-    // var t = mult(modelViewMatrix, instanceMatrix);
-    // gl.uniformMatrix4fv(modelViewMatrixLoc,  false, flatten(t) );
-    gl.drawArrays( gl.TRIANGLES, 0, cylpoints.length/3 );
 
-    // render();
+
+    render();
 }
-
-//----------------------------------------------------------------------------
-
-
-function base() {
-    var s = scale4(BASE_WIDTH, BASE_HEIGHT, BASE_WIDTH);
-    var instanceMatrix = mult( translate( 0.0, 0.5 * BASE_HEIGHT, 0.0 ), s);
-    var t = mult(modelViewMatrix, instanceMatrix);
-    gl.uniformMatrix4fv(modelViewMatrixLoc,  false, flatten(t) );
-    gl.drawArrays( gl.TRIANGLES, 0, cylpoints.length/3 );
-}
-
-//----------------------------------------------------------------------------
-
-
-function upperArm() {
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-    var s = scale4(UPPER_ARM_WIDTH, UPPER_ARM_HEIGHT, UPPER_ARM_WIDTH);
-    var instanceMatrix = mult(translate( 0.0, 0.5 * UPPER_ARM_HEIGHT, 0.0 ),s);
-    var t = mult(modelViewMatrix, instanceMatrix);
-    gl.uniformMatrix4fv( modelViewMatrixLoc,  false, flatten(t) );
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
-}
-
-//----------------------------------------------------------------------------
-
-
-function lowerArm()
-{
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-    var s = scale4(LOWER_ARM_WIDTH, LOWER_ARM_HEIGHT, LOWER_ARM_WIDTH);
-    var instanceMatrix = mult( translate( 0.0, 0.5 * LOWER_ARM_HEIGHT, 0.0 ), s);
-    var t = mult(modelViewMatrix, instanceMatrix);
-    gl.uniformMatrix4fv( modelViewMatrixLoc,  false, flatten(t) );
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
-}
-
-//----------------------------------------------------------------------------
 
 
 var render = function() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
-    modelViewMatrix = rotate(theta[Base], 0, 1, 0 );
-    base();
+    let projectionMatrix = mat4();
+    projectionMatrix = rotateX(45);
+    let projectionMatrixLoc = gl.getUniformLocation(program, 'projectionMatrix');
 
-    // modelViewMatrix = mult(modelViewMatrix, translate(0.0, BASE_HEIGHT, 0.0));
-    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[LowerArm], 0, 0, 1 ));
-    // lowerArm();
-    //
-    // modelViewMatrix  = mult(modelViewMatrix, translate(0.0, LOWER_ARM_HEIGHT, 0.0));
-    // modelViewMatrix  = mult(modelViewMatrix, rotate(theta[UpperArm], 0, 0, 1) );
-    // upperArm();
+    gl.uniformMatrix4fv( projectionMatrixLoc,  false, flatten(projectionMatrix));
+
+    gl.drawArrays(gl.TRIANGLES, 0, points.length)
 
     requestAnimFrame(render);
 }
