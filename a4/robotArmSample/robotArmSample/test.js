b19bdd6ebb636b5ce7615dbd1d026d913134e378
tgrabham <tgrabham@sfu.ca>
2021-04-14 17:02:31 -0700

    finished the cylinder

--- a4/robotArmSample/robotArmSample/test.js
+++ a4/robotArmSample/robotArmSample/test.js
@@ -5,19 +5,22 @@ var canvas, gl, program;
 // Shader transformation matrices
 
 var modelViewMatrix, projectionMatrix;
+var vBuffer, cBuffer;
 
-let cylinder_points = [];
-let cylinder_vertices = [];
-let cylinder_colors = [];
-let circle_points = [];
-let circle_vertices = [];
-let circle_colors = [];
+let points = [];
+let vertices = [];
+let colors = [];
+// let top_circle_points = [];
+// let top_circle_vertices = [];
+// let bottom_circle_points = [];
+// let bottom_circle_vertices = [];
+// let circle_colors = [];
 
-function initCircleVertices (n, vertices){
+function initCircleVertices (n, vertices, y){
   let angle = 0;
   let inc = Math.PI*2/n;
   for(var i=0;i<n;i++){
-    vertices.push(vec4(Math.cos(angle), 0, Math.sin(angle), 1));
+    vertices.push(vec4(Math.cos(angle), y, Math.sin(angle), 1));
     angle += inc;
   }
 }
@@ -33,13 +36,13 @@ function initCircle(points, vertices){
   points.push(vertices[0]);
 }
 
-function initColors(n, colors, r, g, b){
+function initColors(n, r, g, b){
   for(var i=0;i<n;i++){
     colors.push(vec4(r,g,b,1));
   }
 }
 
-function quad(a, b, c, d, points, vertices){
+function quad(a, b, c, d){
   points.push(vertices[a]);
   points.push(vertices[b]);
   points.push(vertices[c]);
@@ -48,7 +51,7 @@ function quad(a, b, c, d, points, vertices){
   points.push(vertices[d]);
 }
 
-function initCylinderVertices(n, vertices){
+function initCylinderVertices(n){
   let angle = 0;
   let inc = Math.PI*2/n;
   for(var i=0;i<n;i++){
@@ -62,12 +65,27 @@ function initCylinderVertices(n, vertices){
   }
 }
 
-function initCylinder(points, vertices) {
-  let n = vertices.length/2;
+function initCylinder(n) {
   for(var i=0;i<n-1;i++){
-    quad(i,i+1,i+n+1,i+n, points, vertices);
+    quad(i,i+1,i+n+1,i+n);
   }
-  quad(n-1,0,n,2*n-1, points, vertices);
+  quad(n-1,0,n,2*n-1);
+  for(var i=0;i<n-1;i++){
+    points.push(vec4(0,1,0,1));
+    points.push(vertices[i]);
+    points.push(vertices[i+1]);
+  }
+  points.push(vec4(0,1,0,1));
+  points.push(vertices[n-1]);
+  points.push(vertices[0]);
+  for(var i=0;i<n-1;i++){
+    points.push(vec4(0,0,0,1));
+    points.push(vertices[n+i]);
+    points.push(vertices[n+i+1]);
+  }
+  points.push(vec4(0,0,0,1));
+  points.push(vertices[2*n-1]);
+  points.push(vertices[n]);
 }
 
 
@@ -114,27 +132,41 @@ window.onload = function init() {
 
     // initCircleVertices(36);
     // initCircle();
-    initCylinderVertices(36, cylinder_vertices);
-    initCylinder(cylinder_points, cylinder_vertices);
-    initColors(cylinder_points.length, cylinder_colors, 1, 0, 0);
+    initCylinderVertices(36);
+    initCylinder(36);
+    initColors(36*6, 1, 0, 0);
+    initColors(36*3, 0, 1, 0);
+    initColors(36*3, 0, 0, 1);
 
-    initCircleVertices(36, circle_vertices);
-    initCircle(circle_points, circle_vertices);
-    initColors(circle_points.length, circle_colors, 0, 0, 1)
+
+    // initCircleVertices(36, top_circle_vertices,1);
+    // initCircle(top_circle_points, top_circle_vertices);
+    // initCircleVertices(36, bottom_circle_vertices,0);
+    // initCircle(bottom_circle_points, bottom_circle_vertices);
+
+    // for(var i=0;i<top_circle_points.length;i++){
+    //   cylinder_points.push(top_circle_points[i]);
+    //   cylinder_colors.push(vec4(0,1,0,1));
+    // }
+    // for(var i=0;i<bottom_circle_points.length;i++){
+    //   cylinder_points.push(bottom_circle_points[i]);
+    //   cylinder_colors.push(vec4(0,0,1,1));
+    // }
+
 
     // Create and initialize  buffer objects
 
-    let vBuffer = gl.createBuffer();
+    vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cylinder_points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    let cBuffer = gl.createBuffer();
+    cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cylinder_colors), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
@@ -146,58 +178,44 @@ window.onload = function init() {
     render();
 }
 
+var delta = 0;
 
 var render = function() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
     let projectionMatrix = mat4();
-    projectionMatrix = rotateX(225);
+    projectionMatrix = rotateX(delta++);
     projectionMatrix = mult(scale4(0.5,0.5,0.5),projectionMatrix);
 
 
     drawCylinder(projectionMatrix);
-    drawCircle(projectionMatrix);
-    let translationMatrix = translate(0,1,0);
-    translationMatrix = rotateX(225);
-    projectionMatrix = mult(scale4(0.5,0.5,0.5),translationMatrix);
-    drawCircle(projectionMatrix);
+
 
     requestAnimFrame(render);
 }
 
 
 function drawCylinder(projectionMatrix){
-  let vBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-  gl.bufferData( gl.ARRAY_BUFFER, flatten(cylinder_points), gl.STATIC_DRAW );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
-  var vPosition = gl.getAttribLocation( program, "vPosition" );
-  gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-  gl.enableVertexAttribArray( vPosition );
-
-  let cBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-  gl.bufferData( gl.ARRAY_BUFFER, flatten(cylinder_colors), gl.STATIC_DRAW );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
 
-  var vColor = gl.getAttribLocation( program, "vColor" );
-  gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-  gl.enableVertexAttribArray( vColor );
 
   let projectionMatrixLoc = gl.getUniformLocation(program, 'projectionMatrix');
 
 
   gl.uniformMatrix4fv( projectionMatrixLoc,  false, flatten(projectionMatrix));
 
-  gl.drawArrays(gl.TRIANGLES, 0, cylinder_points.length)
+  gl.drawArrays(gl.TRIANGLES, 0, points.length)
 }
 
 function drawCircle(projectionMatrix){
-  let vBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
   gl.bufferData( gl.ARRAY_BUFFER, flatten(circle_points), gl.STATIC_DRAW );
 
-  let cBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
   gl.bufferData( gl.ARRAY_BUFFER, flatten(circle_colors), gl.STATIC_DRAW );
 
